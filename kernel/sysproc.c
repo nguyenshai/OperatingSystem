eab324e0c924e7a6bc396063f7debd7f262c1b33
TrienChill <thinhhhhhhhhhhhhhhh@gmail.com>
2026-03-26 21:15:58 +0700

    Add sys_sysinfo function to retrieve system information metrics

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -5,6 +5,7 @@
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
+#include "sysinfo.h"
 
 uint64
 sys_exit(void)
@@ -107,5 +108,26 @@ sys_trace(void)
   argint(0, &mask);       
   myproc()->trace_mask = mask;     
   
+  return 0;
+}
+
+uint64 
+sys_sysinfo(void) 
+{
+  uint64 addr;
+  struct sysinfo info;
+  struct proc *p = myproc();
+  
+  // Fetch the address of the sysinfo struct from the user program
+  argaddr(0, &addr); 
+
+  info.freemem = count_free_mem();
+  info.nproc = count_procs();
+  info.nopenfiles = count_open_files();
+
+  // Copy the sysinfo struct back to the user program
+  if(copyout(p->pagetable, addr, (char *)&info, sizeof(info)) < 0)
+    return -1;
+
   return 0;
 }
