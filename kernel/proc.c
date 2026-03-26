1dbb3b077fad2fc714907f851f0dd094bb32925f
TrienChill <thinhhhhhhhhhhhhhhh@gmail.com>
2026-03-26 21:15:41 +0700

    Add count_procs function to count active processes in the system

--- kernel/proc.c
+++ kernel/proc.c
@@ -695,3 +695,17 @@ procdump(void)
     printf("\n");
   }
 }
+
+uint64 
+count_procs(void) 
+{
+  struct proc *p;
+  uint64 count = 0;
+  for(p = proc; p < &proc[NPROC]; p++) {
+    if(p->state != UNUSED) {
+      count++;
+    }
+  }
+  return count;
+}
+
